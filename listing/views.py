cf0e4d3befdf22b87cd338625c12947c02664f2f
David Drobner <david@drobner.ca>
2023-01-04 14:26:01 -0500

    Remove unused import from view.py

--- listing/views.py
+++ listing/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 from django.forms.models import model_to_dict
 from django.http import HttpResponseRedirect
 from taggit.models import Tag
